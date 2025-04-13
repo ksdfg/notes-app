67c7357e0e4b1292fe5999548898d7648a4c6b45
ksdfg <ksdfg123@gmail.com>
2025-04-13 16:45:07 +0530

    fix(user): obfuscate password when marshalling to JSON
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- models/user.go
+++ models/user.go
@@ -6,5 +6,5 @@ type User struct {
 	gorm.Model
 	Name     string `gorm:"not null" json:"name"`
 	Email    string `gorm:"uniqueIndex;not null" json:"email"`
-	Password string `gorm:"not null" json:"password"`
+	Password string `gorm:"not null" json:"-"`
 }

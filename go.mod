abbfc9087393619b9ca522290e3f756b008d2acb
ksdfg <ksdfg123@gmail.com>
2025-04-13 15:53:37 +0530

    feat(utils): use a colourised text handler in the default logger
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.2
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/lmittmann/tint v1.0.7
 	github.com/spf13/viper v1.20.1
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.37.0
--- utils/logging.go
+++ utils/logging.go
@@ -1,26 +1,19 @@
 package utils
 
 import (
+	"github.com/lmittmann/tint"
 	"log/slog"
 	"os"
 )
 
 // SetDefaultLogger sets the default logger.
 //
-// This sets the default logger to write logs to the console with the given
+// This sets the default logger to write colourised logs to the console with the given
 // level. The logger will include the source of the log message.
 func SetDefaultLogger(level slog.Level) {
 	// Create the logger
 	logger := slog.New(
-		// Use the text handler
-		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-			// Show the source of the log message
-			AddSource: true,
-			// Set the log level
-			Level: level,
-			// Don't replace any attributes
-			ReplaceAttr: nil,
-		}),
+		tint.NewHandler(os.Stdout, &tint.Options{AddSource: true, Level: level}),
 	)
 
 	// Set the default logger

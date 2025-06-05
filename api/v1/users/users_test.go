0ad0ec7075522a799b791c0c9e72f75a9e9d04cb
ksdfg <ksdfg123@gmail.com>
2025-06-05 12:44:34 +0530

    test(user): add tests for login route

--- api/v1/users/users_test.go
+++ api/v1/users/users_test.go
@@ -40,8 +40,25 @@ func (svc MockUserService) Create(user *models.User, opts *service.DBOpts) error
 }
 
 func (svc MockUserService) GetByEmail(email string, opts *service.DBOpts) (models.User, error) {
-	//TODO implement me
-	panic("implement me")
+	if email == "nosuchuser@ksdfg.dev" {
+		return models.User{}, gorm.ErrRecordNotFound
+	}
+
+	password, err := svc.HashPassword("securepassword")
+	if err != nil {
+		return models.User{}, err
+	}
+
+	return models.User{
+		Model: gorm.Model{
+			ID:        1,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		Name:     "Kshitish Deshpande",
+		Email:    "me@ksdfg.dev",
+		Password: password,
+	}, nil
 }
 
 func (svc MockUserService) GetByID(id uint, opts *service.DBOpts) (models.User, error) {
@@ -159,6 +176,110 @@ func (suite *UsersTestSuite) TestRegister() {
 			suite.Equal(tc.output.body.User.Name, responseBody.User.Name)
 			suite.Equal(tc.output.body.User.Email, responseBody.User.Email)
 			suite.Empty(responseBody.User.Password) // Password should not be returned in the response
+		})
+	}
+}
+
+func (suite *UsersTestSuite) TestLogin() {
+	type testCaseOutput struct {
+		status int
+		body   utils.ApiResponse
+	}
+
+	type testCase struct {
+		input  users.LoginRequest
+		output testCaseOutput
+	}
+
+	testCases := map[string]testCase{
+		"successful": {
+			input: users.LoginRequest{
+				Email:    "me@ksdfg.dev",   // Add a valid email here
+				Password: "securepassword", // Add a valid password here
+			},
+			output: testCaseOutput{
+				status: http.StatusOK,
+				body: utils.ApiResponse{
+					Success: true,
+					Message: "User logged in successfully",
+				},
+			},
+		},
+		"user not found": {
+			input: users.LoginRequest{
+				Email:    "nosuchuser@ksdfg.dev",
+				Password: "securepassword",
+			},
+			output: testCaseOutput{
+				status: http.StatusNotFound,
+				body: utils.ApiResponse{
+					Success: false,
+					Message: "User not found",
+				},
+			},
+		},
+		"incorrect password": {
+			input: users.LoginRequest{
+				Email:    "me@ksdfg.dev",
+				Password: "wrongpassword",
+			},
+			output: testCaseOutput{
+				status: http.StatusUnauthorized,
+				body: utils.ApiResponse{
+					Success: false,
+					Message: "Incorrect password",
+				},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		suite.Run(name, func() {
+			// Marshal test user to json []byte body
+			requestBody, err := json.Marshal(tc.input)
+			if err != nil {
+				suite.T().Error(err)
+				return
+			}
+
+			// Generate POST request to /api/v1/users endpoint with body generated above
+			request, err := http.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+			if err != nil {
+				suite.T().Error(err)
+				return
+			}
+
+			// Add content type header so that the app can parse the body
+			request.Header.Add("Content-Type", "application/json")
+
+			// Send the request
+			response, err := suite.app.Test(request)
+			if err != nil {
+				suite.T().Error(err)
+				return
+			}
+			defer response.Body.Close()
+
+			// Assert that the response status code is as expected
+			suite.Equal(tc.output.status, response.StatusCode)
+
+			// Read the response body
+			body, err := io.ReadAll(response.Body)
+			if err != nil {
+				suite.T().Error(err)
+				return
+			}
+
+			// Unmarshal the response body into the expected response type
+			var responseBody utils.ApiResponse
+			if err = json.Unmarshal(body, &responseBody); err != nil {
+				suite.T().Error(err)
+				return
+			}
+
+			// Assert that the response body matches the expected output
+			suite.Equal(tc.output.body.Success, responseBody.Success)
+			suite.Equal(tc.output.body.Message, responseBody.Message)
 		})
 	}
 }
